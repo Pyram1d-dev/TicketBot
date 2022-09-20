1b6b1818f61e98e60187ed9300852a13867efb59
Pyram1d <76640329+Pyram1d@users.noreply.github.com>
2022-09-20 11:25:15 -0500

    Expiration overhaul

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function truncateFloat(num, precision)
     return Math.floor(num * Math.pow(10, precision)) / Math.pow(10, precision);
 }
 
-function setEmbedStyle(embed, user, reactor, desc, style, forceAccepted = false)
+function setEmbedStyle(embed, user, reactor, desc, style, reaction)
 {
     let styleData = embedStyles[style];
     let resultText = styleData.resultText;
@@ -57,6 +57,27 @@ function setEmbedStyle(embed, user, reactor, desc, style, forceAccepted = false)
         resultText = resultText.replace('{username}', user.username);
     if (reactor != null)
         resultText = resultText.replace('{reactorname}', reactor.username);
+    if (reaction != null)
+    {
+        let allreactors = "";
+        let rawlist = reaction.users.cache;
+        let coollist = [];
+        rawlist.forEach(guy => {
+            if (!guy.bot)
+                coollist.push(guy);
+        });
+        for (let i = 0; i < coollist.length; i++) {
+            const element = coollist[i];
+            allreactors += element.username;
+            if (i + 1 < coollist.length)
+            {
+                allreactors += ", ";
+                if (i + 1 == coollist.length - 1)
+                    allreactors += "and ";
+            }
+        }
+        resultText = resultText.replace('{allreactors}', allreactors);
+    }
     embed.setColor(styleData.color)
         .setTitle('Announcement Request')
         .setAuthor({name: user.username, iconURL: user.avatarURL()})
@@ -123,8 +144,41 @@ client.on('interactionCreate', async interaction => {
             });
             msg.react(voteEmojis.accept).then(() => msg.react(voteEmojis.decline).then(() => msg.react(voteEmojis.forceAccept).then(() => msg.react(voteEmojis.ban)))).catch(console.error);
             const filter = (reaction, user) => names.includes(reaction.emoji.name) && !user.bot;
-            // 1,200,000 ms = 20 mins
 
+            let banTime = 600;
+
+            var penis = async function(action, reactor, reaction)
+            {
+                let resultEmbed = new MessageEmbed();
+                msg.reactions.removeAll();
+                switch (action)
+                {
+                    case "Force Accept":
+                    case "Accept":
+                        console.log("Yeah :)");
+                        setEmbedStyle(resultEmbed, user, reactor, desc, `${action === "Force Accept" ? "force" : ""}approved`, reaction);
+                        user.send(`Your request to announce \"${desc}\" has been approved!`);
+                        let announceRole = await fetchRole(member.guild, roleIDs.announcer);
+                        member.roles.add(announceRole);
+                        if (action === "Force Accept")
+                            modCooldowns.set(reactor.id, Date.now());
+                        break;
+                    case 'Ban':
+                        console.log('stfu >>:((');
+                        setEmbedStyle(resultEmbed, user, reactor, desc, "banned", reaction);
+                        user.send(`Your request was fucking stupid and you are unable to request for ${banTime / 60} minutes because of it. #ripbozo`);
+                        cooldowns[user.id] = {startTime: Date.now(), banTime: banTime};
+                        break;
+                    default:
+                        console.log("No >:(");
+                        setEmbedStyle(resultEmbed, user, reactor, desc, "denied", reaction);
+                        user.send(`Your request to announce \"${desc}\" was denied. If you wanted to respond to an announcement, just discuss it in another chat.`);
+                }
+                delete usersRequested[user.id];
+                msg.edit({embeds: [resultEmbed]}).catch(console.error);
+            }
+
+            // 1,200,000 ms = 20 mins
             const collector = msg.createReactionCollector({ filter, time: 1200000 });
 
             collector.on('collect', async (reaction, reactor) => {
@@ -158,45 +212,66 @@ client.on('interactionCreate', async interaction => {
                     return;
                 }
 
-                let resultEmbed = new MessageEmbed();
-                msg.reactions.removeAll();
                 switch (reaction.emoji.name)
                 {
                     case voteEmojiData.forceAccept.name:
                     case voteEmojiData.accept.name:
-                        console.log("Yeah :)");
-                        setEmbedStyle(resultEmbed, user, reactor, desc, `${forceAccepted ? "force" : ""}approved`);
-                        user.send(`Your request to announce \"${desc}\" has been approved!`);
-                        let announceRole = await fetchRole(member.guild, roleIDs.announcer);
-                        member.roles.add(announceRole);
-                        if (forceAccepted)
-                            modCooldowns.set(reactor.id, Date.now());
+                        penis(forceAccepted ? "Force Accept" : "Accept", reactor, reaction);
                         break;
                     case voteEmojiData.ban.name:
-                        console.log('stfu >>:((');
-                        setEmbedStyle(resultEmbed, user, reactor, desc, "banned");
-                        let banTime = 600;
-                        user.send(`Your request was fucking stupid and you are unable to request for ${banTime / 60} minutes because of it. #ripbozo`);
-                        cooldowns[user.id] = {startTime: Date.now(), banTime: banTime};
+                        penis("Ban", reactor, reaction);
                         break;
                     default:
-                        console.log("No >:(");
-                        setEmbedStyle(resultEmbed, user, reactor, desc, "denied");
-                        user.send(`Your request to announce \"${desc}\" was denied. If you wanted to respond to an announcement, just discuss it in another chat.`);
+                        penis("eoawsgfaseiufgewiufg ew", reactor, reaction);
                 }
                 endedOperation = true;
                 collector.stop();
-                delete usersRequested[user.id];
-                msg.edit({embeds: [resultEmbed]}).catch(console.error);
             });
 
             collector.on('end', collected => {
                 if (endedOperation || !(user.id in usersRequested)) return;
                 delete usersRequested[user.id];
+                let col = Array.from(collected.values());
+                let allSameVal = true;
+                let mostReactions = null;
+                let prev = null;
+                col.forEach(reaction => {
+                    if (reaction.emoji.name != voteEmojiData.forceAccept.name)
+                    {
+                        if (prev != null && prev.count != reaction.count)
+                            allSameVal = false;
+                        if (mostReactions == null || reaction.count > mostReactions.count)
+                            mostReactions = reaction;
+                        prev = reaction;
+                    }
+                });
+                if (col.length === 1)
+                {
+                    allSameVal = false;
+                    mostReactions = col[0];
+                }
                 msg.reactions.removeAll();
-                const notCoolEmbed = new MessageEmbed()
-                setEmbedStyle(notCoolEmbed, user, null, desc, "expired");
-                msg.edit({embeds: [notCoolEmbed]}).catch(console.error);
+                if (allSameVal)
+                {
+                    const notCoolEmbed = new MessageEmbed()
+                    setEmbedStyle(notCoolEmbed, user, null, desc, "expired");
+                    msg.edit({embeds: [notCoolEmbed]}).catch(console.error);
+                }
+                else
+                {
+                    switch (mostReactions.emoji.name)
+                    {
+                        case voteEmojiData.forceAccept.name:
+                        case voteEmojiData.accept.name:
+                            penis(mostReactions.emoji.name === voteEmojiData.forceAccept.name ? "Force Accept" : "Accept", null, mostReactions);
+                            break;
+                        case voteEmojiData.ban.name:
+                            penis("Ban", null, mostReactions);
+                            break;
+                        default:
+                            penis("eoawsgfaseiufgewiufg ew", null, mostReactions);
+                    }
+                }
             });
 
             usersRequested[user.id]['cancel'] = () => {
